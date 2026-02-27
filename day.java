121c1cd61b2e4b5e7a2fd07a53bee5b6f1a9acbf
Mohitchourasia999 <mohit.chourasia.cmfeb26@gmail.com>
2026-02-27 15:35:19 +0530

    Add files via upload

---
+++ day.java
@@ -0,0 +1,29 @@
+package assignment2;
+
+public class day {
+
+    public static void main(String[] args) {
+        int d_no=3;
+        switch (d_no) {
+            case 1: System.out.println("Day is Monday");
+                break;
+            case 2: System.out.println("Day is Tuesday");
+                break;
+            case 3: System.out.println("Day is Wednesday");
+                break;
+            case 4: System.out.println("Day is Thursday");
+                break;
+            case 5: System.out.println("Day is Thursday");
+                break;
+            case 6: System.out.println("Day is Thursday");
+                break;
+            case 7: System.out.println("Day is Thursday");
+                break;
+
+            default: System.out.println("invalid day number");
+                break;
+        }
+    }
+    
+}
+
